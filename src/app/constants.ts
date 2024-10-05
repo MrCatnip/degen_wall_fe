669103fd53c8175234fa1edd2d2e99d25399ed5d
MrCatnip <doe69772@gmail.com>
2024-10-05 12:42:02 +0300

    removed unused const

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -28,7 +28,6 @@ export const FETCH_BALANCE_INTERVAL_MS = 20000;
 export const SERVER_URL = "http://localhost:3001";
 export const RPC_URL_KEY = "RPC_URL";
 export const ERASE_PIXELS_CODE = -1;
-export const SQUARE_BORDER_COLOR = "616e96";
 export const USER_REGEX = /^[a-zA-Z0-9_]{1,15}$/;
 // we're using a class because for some reason we need to instantiate a wallet with connection
 // if we want to have access to the IDL constants
