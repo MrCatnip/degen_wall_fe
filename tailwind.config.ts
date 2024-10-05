dfedfd79baecd9bfaaf20b37a5cc4ec7e45c51de
MrCatnip <doe69772@gmail.com>
2024-10-05 16:19:11 +0300

    fixed theme colors bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      color: {
+      colors: {
         "color-1": "var(--color-1)",
         "color-2": "var(--color-2)",
         "color-3": "var(--color-3)",
